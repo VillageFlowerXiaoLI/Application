556fdd47d17023ff802f4a945d50b533d71da829
DUXINLU <duxinlu81@live.com>
2017-05-08 15:05:08 +0800

    已经可以显示用户资料了

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ app = Flask(__name__)
 
 app.secret_key = os.urandom(24)
 
+# app.config.from_object('config')
+
 db_path = '/Users/duxinlu/Desktop/dachuang2017/dachuang.db'
 
 
@@ -31,14 +33,38 @@ def confirm_user(username, password, db):
     cursor = db.cursor()
 
     sql = 'select id,username,password from users where username=?'
-    result = cursor.execute(sql, (username))
+    result = cursor.execute(sql, (username,))
     user = result.fetchall()
 
     if len(user) == 0:
-        return [False, None]
+        return (False, None)
+    elif password == user[0][2]:
+        return (True, user[0][0])
     else:
-        if password == user[0][2]:
-            return (True, user[0])
+        return (False, None)
+
+
+def get_user_info(user_id, db):
+    cursor = db.cursor()
+
+    sql = 'select * from users where id=?'
+    result = cursor.execute(sql, (user_id,))
+    user_info = result.fetchall()
+    user_dict = {}
+
+    if len(user_info) == 0:
+        return None
+    else:
+        # user_dict['id'] = user_info[0][0]
+        user_dict['username'] = user_info[0][1]
+        # user_dict['password'] = user_info[0][2]
+        user_dict['nickname'] = user_info[0][3]
+        user_dict['sex'] = user_info[0][4]
+        user_dict['birthday'] = user_info[0][5]
+        user_dict['email'] = user_info[0][6]
+        user_dict['phnoe_number'] = user_info[0][7]
+        user_dict['head_img'] = user_info[0][8]
+        return user_dict
 
 
 @app.route('/server_shutdown')
@@ -49,7 +75,7 @@ def server_shutdown():
 
 @app.route('/')
 def home_page():
-
+    # 从数据库提取一波文章，传递给模板
     return render_template('home_page.html')
 
 
@@ -67,10 +93,19 @@ def login():
             error = '用户名或密码不正确'
         else:
             session['logged_in'] = True
+            session['user_id'] = login_status[1]
             flash('登录成功')
             return redirect(url_for('home_page'))
 
     return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    session.pop('user_id', None)
+    flash('登出成功')
+    return redirect(url_for('home_page'))
 
 
 @app.route('/articles')
@@ -93,14 +128,22 @@ def travel(travel_id):
 
 @app.route('/user/<user_id>')
 def user_space(user_id):
-    pass
-    '''显示用户的空间'''
+    if not hasattr(g, 'db'):
+        g.db = connect_db()
+
+    user_dict = get_user_info(user_id, g.db)
+
+    print user_id
+    print user_dict
+
+    if user_dict:
+        return render_template('user_space.html', user_dict=user_dict)
 
 
 @app.route('/user/<user_id>/profile')
 def user_profile(user_id):
     pass
-    '''显示用户资料，页面提供修改等操作'''
+    '''用户资料'''
 
 
 if __name__ == '__main__':
